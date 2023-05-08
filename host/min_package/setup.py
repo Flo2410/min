a74c1e1723654b6dc5796ccc0af9b6ecfb9136aa
Flo2410 <florian@hye.dev>
2023-05-08 15:39:14 +0200

    fixed install_requires

--- host/min_package/setup.py
+++ host/min_package/setup.py
@@ -2,10 +2,13 @@ from setuptools import setup
 
 setup(
     name='min',
-    version='2.1',
+    version='2.1.1',
     description='The MIN protocol specification and reference implementation',
     author='min-protocol',
     packages=['min'],
     py_modules=['min', 'minmon', 'PayloadBuilder'],
     url='https://github.com/min-protocol/min',
+    install_requires = [
+        'pyserial>=2.7',
+    ]
 )
